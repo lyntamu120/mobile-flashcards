d6516f04fbdced029d745ea80ba83b0ff5664abb
王希宁 <xiningwang120@gmail.com>
2018-06-05 22:40:18 -0500

    Add feature: Add basic UI for NewDeck

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,14 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 
 class NewDeck extends Component {
   render() {
     return (
-      <View>
-        <Text>new deck</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>What's the title of your new deck?</Text>
+        <TextInput style={styles.textInput} />
+        <TouchableOpacity
+          onPress={this.onSubmit}
+          style={styles.button}
+        >
+          <Text>Submit</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    backgroundColor: '#888',
+    padding: 8,
+    alignItems: 'center',
+    width: 180,
+    marginTop: 30
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 2,
+    marginTop: 40,
+    width: 280
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: 'Cochin',
+    textAlign: 'center'
+  }
+});
 
 export default NewDeck;
